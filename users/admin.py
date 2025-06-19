2d5d150a293ac6afe7b8922f08097a9a6d200263
trmaxim92 <trmaxim92@yandex.ru>
2025-06-20 00:23:54 +0300

    fix models jobs and users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_employer', 'is_worker', 'is_staff')
+    list_filter = ('is_employer', 'is_worker', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Персональная информация', {'fields': ('first_name', 'last_name', 'email', 'phone_number', 'address', 'about', 'profile_picture')}),
+        ('Статусы', {'fields': ('is_employer', 'is_worker', 'is_active', 'is_staff', 'is_superuser')}),
+        ('Рейтинг', {'fields': ('rating',)}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_employer', 'is_worker'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -2,13 +2,40 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    is_employer = models.BooleanField(default=False)
-    is_worker = models.BooleanField(default=False)
-    phone_number = models.CharField(max_length=20, blank=True)
-    address = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
-    rating = models.FloatField(default=0.0)
-    about = models.TextField(blank=True)
+    is_employer = models.BooleanField(
+        default=False,
+        verbose_name="Работодатель"
+    )
+    is_worker = models.BooleanField(
+        default=False,
+        verbose_name="Соискатель"
+    )
+    phone_number = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name="Номер телефона"
+    )
+    address = models.TextField(
+        blank=True,
+        verbose_name="Адрес"
+    )
+    profile_picture = models.ImageField(
+        upload_to='profile_pics/',
+        blank=True,
+        verbose_name="Фото профиля"
+    )
+    rating = models.FloatField(
+        default=0.0,
+        verbose_name="Рейтинг"
+    )
+    about = models.TextField(
+        blank=True,
+        verbose_name="О себе"
+    )
+    
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
     
     def __str__(self):
         return self.username
